04a0d4d97cdab9fbef196a5ee38be48394277dfb
Bradley Lucier <lucier@math.purdue.edu>
2020-01-10 16:23:56 -0500

    Add editor to bottom of document

--- srfi-122.scm
+++ srfi-122.scm
@@ -2186,4 +2186,6 @@ The code uses "(<code>'array-assign!)", "(<code>'specialized-array-share)", "(<c
 (<p> "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.")
 (<p> " THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ")
+(<hr>)
+(<address> "Editor: " (<a> href: "mailto:srfi-editors+at+srfi+dot+schemers+dot+org" "Arthur A. Gleckler"))
 ))))))
